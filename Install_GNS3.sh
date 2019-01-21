6e1d39d46701ce75b33dd0582f4e2aa6a7ce028b
Jose Carlos <42573751+josectheone@users.noreply.github.com>
2019-01-21 16:09:22 -0300

    Update Install_GNS3.sh

--- Install_GNS3.sh
+++ Install_GNS3.sh
@@ -8,10 +8,16 @@ wget -q -O - http://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.a
 sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian bionic non-free contrib" >> /etc/apt/sources.list.d/virtualbox.org.list' &&
 
 echo 'Atualizando listas de pacotes'
-sudo apt update && sudo apt upgrade && sudo apt dist-upgrade && sudo apt autoremove && sudo apt autoclean && sudo apt clean &&
+sudo apt-get -y update &&
+sudo apt-get -y upgrade &&
+sudo apt-get -y dist-upgrade &&
+sudo apt-get -y autoremove &&
+sudo apt-get -y autoclean &&
+sudo apt-get -y clean &&
 
 echo 'Instalando GNS3 e dependencias'
-sudo apt install gns3-gui gns3-iou vpcs iptraf-ng iperf3 ipcalc git vim uml-utilities bridge-utils wireshark wireshark-common wireshark-dev cpulimit qemu qemu-utils qemu-kvm qemu-user qemu-system-x86 virtualbox-5.2 &&
+sudo apt-get -y install gns3-gui gns3-iou vpcs iptraf-ng iperf3 ipcalc git vim uml-utilities bridge-utils wireshark wireshark-common wireshark-dev cpulimit qemu qemu-utils qemu-kvm qemu-user qemu-system-x86 virtualbox tigervnc-viewer software-properties-common &&
+
 echo 'Processo completado, pode iniciar GNS3'
 echo 'Script creado por Josectheone'
 echo '21/01/2019'

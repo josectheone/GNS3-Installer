35488057e1e62b09cc66c6b8b0bc9c1e1d987358
Jose Carlos <42573751+josectheone@users.noreply.github.com>
2019-01-22 08:33:56 -0300

    Update Install_GNS3.sh

--- Install_GNS3.sh
+++ Install_GNS3.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Script para Instalar GNS3 e dependencias em distribuçoes baseadas no debian
 
+echo 'Adicionando suporte a PPA'
+sudo apt-get -y install software-properties-common &&
+
 echo 'Adicionando repositorios e PPA'
 sudo dpkg --add-architecture i386 &&
 sudo add-apt-repository ppa:gns3/ppa &&
@@ -16,7 +19,10 @@ sudo apt-get -y autoclean &&
 sudo apt-get -y clean &&
 
 echo 'Instalando GNS3 e dependencias'
-sudo apt-get -y install gns3-gui gns3-iou vpcs iptraf-ng iperf3 ipcalc git vim uml-utilities bridge-utils wireshark wireshark-common wireshark-dev cpulimit qemu qemu-utils qemu-kvm qemu-user qemu-system-x86 virtualbox tigervnc-viewer software-properties-common &&
+sudo apt-get -y install gns3-gui gns3-iou vpcs iptraf-ng iperf3 ipcalc git vim uml-utilities bridge-utils wireshark wireshark-common wireshark-dev cpulimit qemu qemu-utils qemu-kvm qemu-user qemu-system-x86 virtualbox tigervnc-viewer &&
+
+ECHO 'Agregando usuario nos grupos'
+sudo usermod -a -G ubridge,libvirt,kvm $USER
 
 echo 'Processo completado, pode iniciar GNS3'
 echo 'Script creado por Josectheone'

80b43a4d13dace0dbf0fe5220a9c66e6a0060f20
Jose Carlos <42573751+josectheone@users.noreply.github.com>
2019-03-04 09:04:11 -0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -11,34 +11,34 @@ while true; do
 	read -r -p "Deseja configurar suporte a PPA? [Y/n] " input
 	case $input in
 	[yY][eE][sS] | [yY])
-		if [ -f /usr/sbin/add-apt-repository.sh ]; then
+		if [ -f /usr/sbin/add-apt-repository ]; then
 			break
 		else
-			cat >/usr/sbin/add-apt-repository.sh <<'EOF'
-				#!/bin/bash
-				# SCRIPT add-apt-repository.sh
-				if [ $# -eq 1 ]
-				NM=$(uname -a && date)
-				NAME=$(echo $NM | md5sum | cut -f1 -d" ")
-				then
-				ppa_name=$(echo "$1" | cut -d":" -f2 -s)
-				if [ -z "$ppa_name" ]
-				then
-				echo "PPA name not found"
-				echo "Utility to add PPA repositories in your debian machine"
-				echo "$0 ppa:user/ppa-name"
-				else
-				echo "$ppa_name"
-				echo "deb http://ppa.launchpad.net/$ppa_name/ubuntu bionic main" >> /etc/apt/sources.list
-				apt-get update >> /dev/null 2> /tmp/${NAME}_apt_add_key.txt
-				key=$(cat /tmp/${NAME}_apt_add_key.txt | cut -d":" -f6 | cut -d" " -f3)
-				apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $key
-				rm -rf /tmp/${NAME}_apt_add_key.txt
-				fi
-				else
-				echo "Utility to add PPA repositories in your debian machine"
-				echo "$0 ppa:user/ppa-name"
-				fi
+			cat >/usr/sbin/add-apt-repository <<'EOF'
+#!/bin/bash
+# SCRIPT add-apt-repository.sh
+if [ $# -eq 1 ]
+	NM=$(uname -a && date)
+	NAME=$(echo $NM | md5sum | cut -f1 -d" ")
+then
+	ppa_name=$(echo "$1" | cut -d":" -f2 -s)
+if [ -z "$ppa_name" ]
+then
+	echo "PPA name not found"
+	echo "Utility to add PPA repositories in your debian machine"
+	echo "$0 ppa:user/ppa-name"
+else
+	echo "$ppa_name"
+	echo "deb http://ppa.launchpad.net/$ppa_name/ubuntu bionic main" >> /etc/apt/sources.list
+	apt-get update >> /dev/null 2> /tmp/${NAME}_apt_add_key.txt
+	key=$(cat /tmp/${NAME}_apt_add_key.txt | cut -d":" -f6 | cut -d" " -f3)
+	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $key
+	rm -rf /tmp/${NAME}_apt_add_key.txt
+fi
+else
+	echo "Utility to add PPA repositories in your debian machine"
+	echo "$0 ppa:user/ppa-name"
+fi
 EOF
 			chmod 755 /usr/sbin/add-apt-repository.sh
 		fi
@@ -55,7 +55,7 @@ EOF
 
 done
 
-if [ -f /etc/apt/sources.list.d/gns3-ubuntu-ppa-bionic.list ]; then
+if ls /etc/apt/sources.list.d/gns3-ubuntu-ppa* &> /dev/null; then
 	apt-get update && sudo apt-get install gns3-gui iptables-persistent dnsmasq tigervnc-viewer
 else
 	add-apt-repository ppa:gns3/ppa && apt-get update && apt-get install gns3-gui iptables-persistent dnsmasq tigervnc-viewer
